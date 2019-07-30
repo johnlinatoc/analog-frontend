ee97bf444b914158e3dec1ef1b4b10a6da5acde7
John Linatoc <johnlinatoc1227@gmail.com>
2019-07-30 14:01:24 -0500

    working on arrow pagination

--- src/BoardgameContainer/Boardgame/BoardgameCard.js
+++ src/BoardgameContainer/Boardgame/BoardgameCard.js
@@ -8,12 +8,16 @@ export default class BoardgameCard extends Component {
         const {boardgame} = this.props
         return (
         <div className='boardgame_card'>
-           <img src={boardgame.image}/>
-           <h3>{boardgame.name}</h3>
+          <div className='img-container'>
+            <img src={boardgame.image}/>
+          </div>
+          <div className='info-container'>
+            <h3>{boardgame.name}</h3>
             <p>
-               <span>{boardgame.price}</span>
+              <span>{boardgame.price}</span>
             </p>
             <button onClick={()=> this.props.addToCart(boardgame.id)}>Add to Cart</button>
+          </div>
         </div>
          );
     }
--- src/BoardgameContainer/BoardgameContainer.js
+++ src/BoardgameContainer/BoardgameContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import BoardgameCard from './Boardgame/BoardgameCard'
-
+import arrows from './images/arrow.png'
+import './Boardgame/Boardgame.css'
 
 
 export default class BoardgameContainer extends Component {
   constructor(props){
     super(props)
     this.state = {
-      page: 1,
+      page: 0,
       // boardgames: this.props.boardgames,
       lastNineBoardgames: [],
     }
@@ -19,6 +20,16 @@ export default class BoardgameContainer extends Component {
 // this.setState({ lastNineBoardgames: lastNineBoardgames })
 // }
 
+  pageForward = (e) => {
+    e.preventDefault()
+    let start = this.state.page;
+    start += 1
+    this.setState({
+      page: start
+    })
+    console.log('working')
+  }
+
   render() {
       //let boardgames = this.state.boardgames
       const start = 10 * this.state.page
@@ -28,6 +39,12 @@ export default class BoardgameContainer extends Component {
               {boardgames.map( boardgame => {
                   return <BoardgameCard boardgame={boardgame} addToCart={this.props.addToCart}/>
               })}
+              <div className='pagination'>
+                <img src={arrows} className="arrows"/>
+
+                <a className="page-back"> left </a>
+                <a onClick={(e)=>this.pageForward(e)} className="page-forward"> right </a>
+              </div>
           </div>
        );
   }
