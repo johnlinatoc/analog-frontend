f1dba03586ca8fdd8db29351f4521e43009abf0e
John Linatoc <johnlinatoc1227@gmail.com>
2019-07-30 18:01:53 -0500

    initial work on shopping cart css

--- src/CheckoutContainer/Cart/Cart.js
+++ src/CheckoutContainer/Cart/Cart.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-
+import './cart.css'
 
 export default class Cart extends Component {
-    
-    render() { 
+
+    render() {
 
       const {boardgame} = this.props
-        return ( 
-            <div>
+        return (
+        <div>
             <img src={boardgame.image}/>
-            <h3>{boardgame.name}</h3>
+            <h3>Name: {boardgame.name}</h3>
+            <p>Quantity: {boardgame.quantity}</p>
              <p>
-                <span>{boardgame.price * boardgame.quantity}</span> 
+                <span>Price: {boardgame.price * boardgame.quantity}</span>
              </p>
              <button onClick={()=> this.props.subtractFromCart(boardgame.id)}>-</button>
              <button onClick={()=> this.props.addToCart(boardgame.id)}>+</button>
--- src/CheckoutContainer/CartContainer.js
+++ src/CheckoutContainer/CartContainer.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import Cart from './Cart/Cart'
 import Payment from './Cart/Payment'
+import './Cart/cart.css'
+import Button from './Cart/images/continue.png'
+import Line from './Cart/images/line.png'
 export default class CartContainer extends Component {
-    
-    render() { 
+
+    render() {
         const cartItems = this.props.cart
         let totalCost = cartItems.reduce((accumulator, boardgame) => (boardgame.price * boardgame.quantity) + accumulator, 0)
-        return ( 
+        return (
             <div>
-                {cartItems.map( boardgame => {
+              <div className='cart-container'>
+                <h1 className='title-container'>Shopping Cart</h1>
+                <div>
+                  <ul className='header-container'>
+                    <li>Boardgame</li>
+                    <li>Quantity</li>
+                    <li>Price</li>
+                  </ul>
+                </div>
+                <div className='bag-container'>
+                  {cartItems.map( boardgame => {
                     return <Cart boardgame={boardgame} addToCart={this.props.addToCart} subtractFromCart={this.props.subtractFromCart} removeFromCart={this.props.removeFromCart} totalCost={totalCost}/>
-                })}
-                <p>
-                    <span>Total Cost: {totalCost}</span> 
-                </p>
-                <button onClick={()=> this.props.checkout()}>Checkout</button>
+                  })}
+                </div>
+                <div className='continue-button'>
+                  <img src={Button}/>
+                </div>
+                <div className='total-container'>
+                  <img src={Line} />
+                  <p>
+                    <span>Total Cost: {totalCost}</span>
+                  </p>
+                </div>
+              </div>
+              <button onClick={()=> this.props.checkout()}>Checkout</button>
                 <Payment/>
             </div>
          );
