9091de1cbba1d989ddfd4c874d9495638b851110
Joel Seng <joel29dec@gmail.com>
2019-07-30 18:08:59 -0500

    css more

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
         handleLogin={(user) => {this.handleLogin(user)}}/>
       }} />
       <Route exact path="/" render={() => {
-        return <div style={{border: '2px solid green', width: '100%', height: '100em'}} >
+        return <div >
             <HeaderWidget />
             <BoardgameContainer boardgames={this.state.boardgames} addToCart={(id) => {this.addToCart(id)}}/>
           </div>
